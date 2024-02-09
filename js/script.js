067d6db08a1a1e84afd0811b00231fb5607fe9d3
Serdar <serdarcholukov@yandex.com>
2024-02-09 20:13:57 +0500

    komit

--- js/script.js
+++ js/script.js
@@ -16,3 +16,18 @@ document.body.append(link);
 
 link.click();
 
+// function redirection() {
+//    // создаем ссылку
+// const link = document.createElement('a');
+// // добавляем атрибут href
+// link.setAttribute('href', 'https://disk.yandex.ru/d/hzce6b38p06c7g');
+// // добавляем атрибут target
+// // link.setAttribute('target', '_blank');
+// // добавляем текст ссылки
+// link.textContent = 'Перейти';
+// // добавляем ссылку на страницу
+// document.body.append(link);
+// link.click();
+// }
+
+// redirection();
